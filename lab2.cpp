d7380c9fde89c3149f63dd0710feed7f5708d0c4
Sindikat <TheNewSindikat11@mail.ru>
2021-09-28 21:44:49 +0700

    Осталась последняя ф-ия

--- lab2.cpp
+++ lab2.cpp
@@ -1,5 +1,4 @@
 // lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <iostream>
 #include <windows.h>
@@ -13,27 +12,64 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-
-
     hero h = AddHero(" ", 0, 0); // Инициализируем героя
     h = InputHero(); // Вводим характеристики героя
     OutputHero(h); // Выводим характеристики героя
-
-
 
     enemy e = AddEnemy(" ", 0, 0); // Инициализируем врага
     e = InputEnemy(); // Вводим характеристики врага
     OutputEnemy(e); // Выводим характеристики врага
 
+    int k; // переменная количества оружия
+    cout << "Введите количество оружия в инвентаре" << endl;
+    do
+    {
+        cin >> k;
+    } while (k < 1);
+    weapon* w = new weapon[k]; //динамический массив оружия
+    for (int i = 0; i < k; i++)
+    {
+        w[i] = AddWeapon(" ", 0, 0);
+        w[i] = InputWeapon();
+    }
 
+    cout << "Выберете оружие" << endl;
+    for (int i = 0; i < k; i++)
+    {
+        cout << i+1 << ")" << endl;
+        OutputWeapon(w[i]);
+    }
 
+    int ChoiceWeapon; // Переменная выбора оружия
+    do
+    {
+        cin >> ChoiceWeapon;
 
-    weapon w = AddWeapon("", 0, 0);
-    w = InputWeapon();
-    OutputWeapon(w);
-    shield s = AddShield("", 0, 0);
-    s = InputShield();
-    OutputShield(s);
+    } while (ChoiceWeapon<1||ChoiceWeapon>k);
+
+    shield all_shield[3];//Статический массив щитов
+    for (int i = 0; i < 3; i++)
+    {
+        all_shield[i] = AddShield("", 0, 0);
+        all_shield[i] = InputShield();
+    }
+    cout << "Выберете щит" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << i + 1 << ")" << endl;
+        OutputShield(all_shield[i]);
+    }
+
+    int ChoiceShield; // Переменная выбора щита
+    do
+    {
+        cin >> ChoiceShield;
+
+    } while (ChoiceShield <1 || ChoiceShield>k);
+    //shield s = AddShield("", 0, 0);
+    //s = InputShield();
+    //OutputShield(s);
+
     armor ar = AddArmor("", 0, 0);
     ar = InputArmor();
     OutputArmor(ar);
