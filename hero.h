615da74a3b36a97fab71827e9890f9f4430ff104
Sindikat <TheNewSindikat11@mail.ru>
2021-10-07 18:11:41 +0700

    Пытаюсь сделать класс Hero на основе структуры hero

--- hero.h
+++ hero.h
@@ -7,6 +7,19 @@
 #include "weapon.h"
 using namespace std;
 
+class Hero
+{
+public:
+	hero initializeHero(string name, int strenght, int hp);
+	hero inputHero(Armor armor, Weapon weapon, Shield shield);
+	void outputHero(Hero hero);
+	int Fight(Hero hero, Enemy enemy);
+private:
+	string name_;
+	int strenght;
+	int hp;
+};
+/*
 typedef struct
 {
 	string name;
@@ -16,7 +29,8 @@ typedef struct
 	weapon my_weapon;
 	shield my_shield;
 } hero;
+*/
 hero InitHero(string a, int b, int c);
 hero InputHero(armor* b, weapon* c, shield* d);
-void OutputHero(hero b);/*функция вывода переменной*/
+void OutputHero(hero b);//функция вывода переменной
 int Fight(hero a, enemy b);
